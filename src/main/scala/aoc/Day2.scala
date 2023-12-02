0f0e96660867be101eba4e2a5f44d61810d79f44
Dan Staby <dstaby@spotify.com>
2023-12-02 07:54:19 +0100

    Clean up day 2

--- src/main/scala/aoc/Day2.scala
+++ src/main/scala/aoc/Day2.scala
@@ -12,19 +12,19 @@ object Day2 extends App with Common {
         .map(_.split(", ").map { case s"$count $color" => color -> count.toInt }.toSeq.toMap)
   }
 
-  def partOne(input: Seq[String]): Int = {
+  def getMaxCubes(game: Seq[Map[String, Int]]): Map[String, Int] =
+    game.flatten.groupMapReduce(_._1)(_._2)(max)
 
+  def partOne(input: Seq[String]): Int = {
     val maxCubes = Map("red" -> 12, "green" -> 13, "blue" -> 14)
 
     parseInput(input).zipWithIndex.collect {
-      case (game, id) if game.flatten.groupMapReduce(_._1)(_._2)(max).toSeq.toMap.forall {
-            case (color, count) => count <= maxCubes(color)
-          } =>
-        id + 1
+      case (game, id) if getMaxCubes(game).forall { case (cr, ct) => ct <= maxCubes(cr) } => id + 1
     }.sum
   }
+
   def partTwo(input: Seq[String]): Int = parseInput(input).map { game =>
-    game.flatten.groupMapReduce(_._1)(_._2)(max).values.product
+    getMaxCubes(game).values.product
   }.sum
 
   println("Part 1: " + partOne(problemInput))
