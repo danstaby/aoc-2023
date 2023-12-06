b1df4a79d7f5f22721b12017a6d35d2d16710323
Dan Staby <dstaby@spotify.com>
2023-12-06 09:34:01 +0100

    Fix off by one error in day 6

--- src/main/scala/aoc/Day6.scala
+++ src/main/scala/aoc/Day6.scala
@@ -6,32 +6,25 @@ object Day6 extends App with Common {
     nums.trim.split("[ ]+").map(_.toInt).toSeq
   }
 
-  def partOne(input: Seq[String]): Int = {
-    val races = parseInput(input).transpose
-
-    races.map { case Seq(time, distanceMax) =>
+  def partOne(input: Seq[String]): Int =
+    parseInput(input).transpose.map { case Seq(time, distanceMax) =>
       1 to time map (t => t * (time - t)) count (_ > distanceMax)
     }.product
-  }
 
-  def partTwo(input: Seq[String]): Long = {
-    val races = parseInput(input).map(_.map(_.toString).reduce(_ + _).toLong)
-
-    /*
+  /*
     The solution to the equation t * (time - t) = distanceMax is:
     t = (time +- sqrt(time^2 - 4 * distanceMax)) / 2
 
     Find the number if integer times between solution t0 and t1:
-     */
-
-    races match {
+   */
+  def partTwo(input: Seq[String]): Long =
+    parseInput(input).map(_.map(_.toString).reduce(_ + _).toLong) match {
       case Seq(time, distanceMax) =>
-        val t0 = (time + math.sqrt(time * time - 4 * distanceMax)) / 2
-        val t1 = (time - math.sqrt(time * time - 4 * distanceMax)) / 2
+        val t0 = (time + math.sqrt(time * time - 4 * (distanceMax + 1))) / 2
+        val t1 = (time - math.sqrt(time * time - 4 * (distanceMax + 1))) / 2
 
         Math.floor(t0).toLong - Math.ceil(t1).toLong + 1
     }
-  }
 
   println("Part 1: " + partOne(problemInput))
   println("Part 2: " + partTwo(problemInput))
