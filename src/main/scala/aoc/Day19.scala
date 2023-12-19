97e945d12b5f3a4e3b6fa89adec2eeb23ae59524
Dan Staby <dstaby@spotify.com>
2023-12-19 10:27:22 +0100

    Clean day 19

--- src/main/scala/aoc/Day19.scala
+++ src/main/scala/aoc/Day19.scala
@@ -1,31 +1,6 @@
 package aoc
 
-import scala.annotation.tailrec
-import scala.util.control.Breaks._
-
 object Day19 extends App with Common {
-
-  /*
-  Example data
-
-  px{a<2006:qkq,m>2090:A,rfg}
-  pv{a>1716:R,A}
-  lnx{m>1548:A,A}
-  rfg{s<537:gd,x>2440:R,A}
-  qs{s>3448:A,lnx}
-  qkq{x<1416:A,crn}
-  crn{x>2662:A,R}
-  in{s<1351:px,qqz}
-  qqz{s>2770:qs,m<1801:hdj,R}
-  gd{a>3333:R,R}
-  hdj{m>838:A,pv}
-
-  {x=787,m=2655,a=1222,s=2876}
-  {x=1679,m=44,a=2067,s=496}
-  {x=2036,m=264,a=79,s=2244}
-  {x=2461,m=1339,a=466,s=291}
-  {x=2127,m=1623,a=2188,s=1013}
-   */
 
   def parseInput(input: Seq[String]): (Map[String, Workflow], Seq[Map[String, Int]]) =
     input.mkString("\n").split("\n\n").toList match {
@@ -86,9 +61,7 @@ object Day19 extends App with Common {
       val complement = parts(p).relativeComplement(r).filter(_.nonEmpty)
       complement.map(newr => countAcceptedWorkflow(t, workflows, parts.updated(p, newr))).sum +
         intersection
-          .map(i =>
-            countAcceptedWorkflow(workflows(n).conditions.toList, workflows, parts.updated(p, i))
-          )
+          .map(i => countAcceptedWorkflow(workflows(n).conditions, workflows, parts.updated(p, i)))
           .sum
   }
 
